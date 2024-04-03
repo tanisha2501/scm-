6c5155d3799a6e6bd9705b50d69bb194be92aa97
tanisha2501 <155954445+tanisha2501@users.noreply.github.com>
2024-04-03 19:21:30 +0530

    Create tanisha.scm

---
+++ tanisha.scm
@@ -0,0 +1,17 @@
+int main()
+ {
+     int num1,num2;
+     printf("enter the two number:");
+     scanf("%d%d",&num1,&num2);
+     switch(num1>num2)
+     {
+         case 0:
+         printf("%d is maximum",num2);
+         break;
+
+        case 1:
+         printf("%d is maximum",num1);
+         break;
+     }
+     return 0;
+ }
